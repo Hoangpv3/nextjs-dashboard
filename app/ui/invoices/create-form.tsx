b667e0a153b94331ef9a35ab0102fd9566ae3128
Hoangpv3 <hoang.pv208000@sis.hust.edu.vn>
2024-12-12 13:43:03 +0700

    3rd commit

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,13 +1,16 @@
-import { CustomerField } from '@/app/lib/definitions';
-import Link from 'next/link';
+'use client';
+
+import { useActionState } from "react";
+import { CustomerField } from "@/app/lib/definitions";
+import Link from "next/link";
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/lib/action';
+} from "@heroicons/react/24/outline";
+import { Button } from "@/app/ui/button";
+import { createInvoice } from "@/app/lib/action";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
@@ -87,6 +90,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  required
                 />
                 <label
                   htmlFor="paid"
